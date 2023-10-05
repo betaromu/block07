4dec949dac577649e213d1eb5f0de8e60e5f8aa1
Brian <hijoputo@Brians-MacBook-Air.local>
2023-10-05 11:04:51 -0700

    third commit

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-// This lock has three separate numbers in its combination. All the numbers must match to open the lock by running each function.
+// This lock has three separate numbers in its combination. All the numbers must match to open the lock by running each function. Enter the numbers in order to open the lock.
 
 const instructions = "You have received this message because you have chosen to open an important vault. Here is the secret combination:";
 console.log(instructions)
 alert(instructions);
 
+// Enter the follow number first
 let firstcode = 5+(2+3);
 console.log(firstcode);
 alert(firstcode);
 
+// Enter the next number second
 let secondcode = (2+2)*10;
 console.log(secondcode);
 alert(secondcode);
 
+// Enter the next number last
 let thirdcode = (80/2)-1;
 console.log(thirdcode);
 alert(thirdcode);
