656cd4ee36cd5e3f676e0dde9b5ef94524a21de0
Brian <hijoputo@Brians-MacBook-Air.local>
2023-10-05 10:59:59 -0700

    secodn commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// This lock has three separate numbers in its combination. All the numbers must match to open the lock by running each function.
+
+const instructions = "You have received this message because you have chosen to open an important vault. Here is the secret combination:";
+console.log(instructions)
+alert(instructions);
+
+let firstcode = 5+(2+3);
+console.log(firstcode);
+alert(firstcode);
+
+let secondcode = (2+2)*10;
+console.log(secondcode);
+alert(secondcode);
+
+let thirdcode = (80/2)-1;
+console.log(thirdcode);
+alert(thirdcode);
+
---
+++ index2.js
@@ -0,0 +1,75 @@
+let favoriteNumber
+
+console.log(favoriteNumber)
+
+favoriteNumber = 3
+
+console.log(favoriteNumber)
+
+let myName= 'Brian'
+
+console.log(myName)
+
+myName = 'Rick'
+
+console.log(myName)
+
+const gravity = 9.8
+
+let world = 'World'
+
+console.log(hello+world) // String Concatenation
+console.log(`${hello}, ${world}!`) // Template Literal
+
+let mystery = 'some value'
+let mystery2 = 'true' + true + hello
+console.log('The datatype of mystery: ', typeof mystery)
+console.log('The datatype of myster2: ', typeof mystery2)
+console.log(typeof (null +45))
+console.log(typeof (typeof 42))
+
+console.log(123456 % 13)
+
+console.log(1 + 4 / 2) // PEMDAS
+
+// && and
+// || or
+// ! - bang (not)
+
+let boolean = true
+
+console.log(boolean)
+console.log(!boolean)
+
+console.log(Boolean(boolean && 'Hello'))
+
+// Zero is false
+// Any other number is true
+
+// Null and Undefined are also false
+
+// Use !! to coerce anything into a boolean
+
+console.log('Not Hello: ', !!'hello')
+
+let numCows = 50;
+
+const x = "Hello world";
+
+console.log("Hellow World");
+
+console.log("Hello ," + "Disney Land!");
+
+let hello = 'Hello'
+let disney = 'Disney Land';
+
+console.log(`${hello}, ${disney}!`)
+
+const numUmbrellas = 10*3;
+
+const num = 5+1;
+console.log(num);
+
+
+
+
